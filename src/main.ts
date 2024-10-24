50fdc962e2ab565fb8688254fb99f86c463473c5
hirakm0ventures <hirakroya50@gmail.com>
2024-10-24 14:21:33 +0530

    init

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+dotenv.config();
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth Microservice')
+    .setDescription(' API description')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
