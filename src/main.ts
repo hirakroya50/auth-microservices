1d3718d31dc1ad962bc287f163b869d0778117b8
hirakm0ventures <hirakroya50@gmail.com>
2024-11-16 22:34:39 +0530

    add cros

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,14 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
+  //CROS policy
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow requests from your frontend's origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Accept', // Allowed headers
+    credentials: true, // Allow credentials (cookies)
+  });
+
   const port = process.env.PORT ?? 3001;
   await app.listen(port);
   console.log('Port----', port);
