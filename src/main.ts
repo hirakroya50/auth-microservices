2521fb96e806b0e67b3903a3404343c3f51f75a3
hirakroya50 <hirakroya50@gmail.com>
2025-01-14 21:54:08 +0530

    update cors header

--- src/main.ts
+++ src/main.ts
@@ -27,9 +27,9 @@ async function bootstrap() {
 
   //CROS policy
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow requests from your frontend's origin
+    origin: ['http://localhost:3000', 'http://localhost:5173'], // Allow requests from your frontend's origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
-    allowedHeaders: 'Content-Type, Accept', // Allowed headers
+    allowedHeaders: 'Content-Type, Accept, Authorization', // Allowed headers
     credentials: true, // Allow credentials (cookies)
   });
 
